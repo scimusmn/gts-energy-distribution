3cce5ab414872a6f66a4137ae8d18d4277ad46ad
eojreyem <jmeyer@smm.org>
2021-04-14 10:48:37 -0500

    shift in restructure

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -30,64 +30,38 @@ const int gas2_btn_down_pin = 12;
 SerialController serialController;
 const long baudrate = 115200;
 
-const char str_1[] PROGMEM = "gas-1-jack";     // shift in board 1
-const char str_2[] PROGMEM = "gas-2-jack";     // 2
-const char str_3[] PROGMEM = "solar-1-jack";     // 3
-const char str_4[] PROGMEM = "solar-2-jack";     // 4
-const char str_5[] PROGMEM = "solar-3-jack";     // 5
-const char str_6[] PROGMEM = "wind-1-jack";     // 6
-const char str_7[] PROGMEM = "wind-2-jack";     // 5
-const char str_8[] PROGMEM = "wind-3-jack";     // 6
-const char str_9[] PROGMEM = "coal-1-switch";      // 7
-const char str_10[] PROGMEM = "coal-2-switch";      // 8
-//const char str_9[] PROGMEM = "hydro-1-jack";    // 9
-//const char str_10[] PROGMEM = "hydro-2-jack";   // 10
-const char str_11[] PROGMEM = "coal-3-switch";   // 11
-const char str_12[] PROGMEM = "coal-4-switch";   // 12
-const char str_13[] PROGMEM = "coal-1-jack";   // 13
-const char str_14[] PROGMEM = "coal-2-jack";    // 14
-const char str_15[] PROGMEM = "coal-3-jack";    // 15
-const char str_16[] PROGMEM = "coal-4-jack";    // 16
-//const char str_17[] PROGMEM = "coal-1-switch";  // 17
-//const char str_18[] PROGMEM = "coal-2-switch";  // 18
-//const char str_19[] PROGMEM = "coal-3-switch";  // 19
-//const char str_20[] PROGMEM = "coal-4-switch";  // 20
+const char str_1[] PROGMEM = "coal-1-switch";   // shift in board 1
+const char str_2[] PROGMEM = "coal-2-switch";   // 2
+const char str_3[] PROGMEM = "coal-3-switch";   // 3
+const char str_4[] PROGMEM = "coal-4-switch";   // 4
+const char str_5[] PROGMEM = "coal-1-jack";     // 5
+const char str_6[] PROGMEM = "coal-2-jack";     // 6
+const char str_7[] PROGMEM = "coal-3-jack";     // 5
+const char str_8[] PROGMEM = "coal-4-jack";     // 6
+const char str_9[] PROGMEM = "coal-5-jack";     // 7
+const char str_10[] PROGMEM = "coal-6-jack";    // 8
+const char str_11[] PROGMEM = "coal-5-switch";  // 11
+const char str_12[] PROGMEM = "coal-6-switch";  // 12
+const char str_13[] PROGMEM = "gas-1-jack";     // 13
+const char str_14[] PROGMEM = "gas-2-jack";     // 14
+const char str_15[] PROGMEM = "hydro-1-jack";   // 15
+const char str_16[] PROGMEM = "hydro-2-jack";   // 16
+const char str_17[] PROGMEM = "solar-1-jack";   // 17
+const char str_18[] PROGMEM = "solar-2-jack";   // 18
+const char str_19[] PROGMEM = "solar-3-jack";   // 19
+const char str_20[] PROGMEM = "wind-1-jack";    // 20
+const char str_21[] PROGMEM = "wind-2-jack";    // 21
+const char str_22[] PROGMEM = "wind-3-jack";    // 22
+const char str_23[] PROGMEM = "no-connection";  // 23
+const char str_24[] PROGMEM = "no-connection";  // 24
 
 // Table to refer to strings.
 const char *const string_table[] PROGMEM = {
-    str_1,  str_2,  str_3,  str_4,  str_5,  str_6,  str_7,
-    str_8,  str_9,  str_10, str_11, str_12, str_13, str_14,
-    str_15, str_16};
+    str_17, str_18, str_19, str_20, str_21, str_22, str_23, str_24,
+    str_9,  str_10, str_11, str_12, str_13, str_14, str_15, str_16,
+    str_1,  str_2,  str_3,  str_4,  str_5,  str_6,  str_7,  str_8};
 
 char buffer[15];
-
-// // message associated with the state of each shift in bit.
-// char *shiftInStrings[] = {
-//     "coal-1-jack",    // 1
-//     "coal-2-jack",    // 2
-//     "coal-3-jack",    // 3
-//     "coal-4-jack",    // 4
-//     "gas-1-jack",     // 5
-//     "gas-2-jack",     // 6
-//     "gas-3-jack",     // 7
-//     "gas-4-jack",     // 8
-//     "hydro-1-jack",   // 9
-//     "hydro-2-jack",   // 10
-//     "hydro-3-jack",   // 11
-//     "hydro-4-jack",   // 12
-//     "wind-1-jack",    // 13
-//     "wind-2-jack",    // 14
-//     "wind-3-jack",    // 15
-//     "wind-4-jack",    // 16
-//     "solar-1-jack",   // 17
-//     "solar-2-jack",   // 18
-//     "solar-3-jack",   // 19
-//     "solar-4-jack",   // 20
-//     "coal-1-switch",  // 21
-//     "coal-2-switch",  // 22
-//     "coal-3-switch",  // 23
-//     "coal-4-switch",  // 24
-// };
 
 long cableStates, prevCableStates;
 
@@ -104,11 +78,11 @@ SerialButton buttons[] = {
     SerialButton(&serialController, "gas-2-button-up", gas2_btn_up_pin)};
 
 // Array of levers
-int NUMBER_OF_LEVERS = 3;
+int NUMBER_OF_LEVERS = 2;
 SerialAnalog levers[] = {
     SerialAnalog(&serialController, "hydro-1-lever", hydro_1_input_pin, 50),
     SerialAnalog(&serialController, "hydro-2-lever", hydro_2_input_pin, 50),
-    SerialAnalog(&serialController, "hydro-3-lever", hydro_3_input_pin, 50)};
+};
 
 void setup() {
   // Ensure Serial Port is open and ready to communicate
@@ -151,22 +125,19 @@ void updateJacksSwitches() {
   // the register attached to the chip comes in first
   statesIn = shiftIn(shift_in_data_pin, shift_in_clock_pin);
   cableStates = statesIn;
+
   statesIn = shiftIn(shift_in_data_pin, shift_in_clock_pin);
   cableStates = cableStates << 8;
   cableStates = cableStates | statesIn;
-//
-//  statesIn = shiftIn(shift_in_data_pin, shift_in_clock_pin);
-//  cableStates = cableStates << 8;
-//  cableStates = cableStates | statesIn;
-//
-//  statesIn = shiftIn(shift_in_data_pin, shift_in_clock_pin);
-//  cableStates = cableStates << 8;
-//  cableStates = cableStates | statesIn;
+
+  statesIn = shiftIn(shift_in_data_pin, shift_in_clock_pin);
+  cableStates = cableStates << 8;
+  cableStates = cableStates | statesIn;
 
   if (prevCableStates != cableStates)  // if the states changed...
   {
     long mask = 1;                // create a mask
-    for (int n = 0; n < 16; n++)  // iterate through each bit
+    for (int n = 0; n < 24; n++)  // iterate through each bit
     {
       // check if the bit changed since last state change.
       if ((mask & prevCableStates) != (mask & cableStates)) {
@@ -237,27 +208,25 @@ void onParse(char *message, char *value) {
     lightBarGraph(6, atoi(value));  // value of first pixel to be lit.
   else if (strcmp(message, "gas-2-light-bar") == 0)
     lightBarGraph(14, atoi(value));  // value of first pixel to be lit.
-//  else if (strcmp(message, "gas-3-light-bar") == 0)
-//    lightBarGraph(22, atoi(value));  // value of first pixel to be lit.
 
   else if (strcmp(message, "hydro-1-light-bar") == 0)
     lightBarGraph(22, atoi(value));  // value of first pixel to be lit.
-//  else if (strcmp(message, "hydro-2-light-bar") == 0)
-//    lightBarGraph(38, atoi(value));  // value of first pixel to be lit.
+  else if (strcmp(message, "hydro-2-light-bar") == 0)
+    lightBarGraph(30, atoi(value));  // value of first pixel to be lit.
 
   else if (strcmp(message, "solar-1-light-bar") == 0)
-    lightBarGraph(30, atoi(value));  // value of first pixel to be lit.
-  else if (strcmp(message, "solar-2-light-bar") == 0)
     lightBarGraph(38, atoi(value));  // value of first pixel to be lit.
-  else if (strcmp(message, "solar-3-light-bar") == 0)
+  else if (strcmp(message, "solar-2-light-bar") == 0)
     lightBarGraph(46, atoi(value));  // value of first pixel to be lit.
+  else if (strcmp(message, "solar-3-light-bar") == 0)
+    lightBarGraph(54, atoi(value));  // value of first pixel to be lit.
 
   else if (strcmp(message, "wind-1-light-bar") == 0)
-    lightBarGraph(54, atoi(value));  // value of first pixel to be lit.
-  else if (strcmp(message, "wind-2-light-bar") == 0)
     lightBarGraph(62, atoi(value));  // value of first pixel to be lit.
-  else if (strcmp(message, "wind-3-light-bar") == 0)
+  else if (strcmp(message, "wind-2-light-bar") == 0)
     lightBarGraph(70, atoi(value));  // value of first pixel to be lit.
+  else if (strcmp(message, "wind-3-light-bar") == 0)
+    lightBarGraph(78, atoi(value));  // value of first pixel to be lit.
 
   else if (strcmp(message, "get-all-states") == 0)  // TODO
   {
