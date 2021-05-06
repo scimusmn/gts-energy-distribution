31852d72895b0970b0727d50bc4c3083844c4c02
trig-vee <tnordberg@smm.org>
2021-05-05 22:58:56 -0500

    Sun cycle reset on session start

--- src/components/DayCycle/Sun/index.js
+++ src/components/DayCycle/Sun/index.js
@@ -6,22 +6,27 @@ function Sun({ duration, animOffset, paused }) {
 
   useEffect(() => {
     if (!paused) {
-      sunRef.current.style.animation = 'none';
-      setTimeout(() => {
-        sunRef.current.style.animation = '';
-      }, 15);
+      // sunRef.current.style.animation = 'none';
+      // setTimeout(() => {
+      //   sunRef.current.style.animation = '';
+      // }, 15);
     }
   }, [paused]);
 
   return (
-    <div
-      className="sun"
-      ref={sunRef}
-      style={{
-        animationDuration: `${duration}s`,
-        animationDelay: `${animOffset}s`,
-      }}
-    />
+    <>
+      {!paused
+      && (
+      <div
+        className="sun"
+        ref={sunRef}
+        style={{
+          animationDuration: `${duration}s`,
+          animationDelay: `${animOffset}s`,
+        }}
+      />
+      )}
+    </>
   );
 }
 
--- src/components/ReadyScreen/index.js
+++ src/components/ReadyScreen/index.js
@@ -6,28 +6,30 @@ import {
 
 const ReadyScreen = () => (
   <Modal isOpen size="xl">
-    <ModalHeader><h1>Power the city!</h1></ModalHeader>
+    <ModalHeader><h1>POWER THE CITY</h1></ModalHeader>
     <ModalBody>
-      <h3>Can you power the city for 3 days?</h3>
       <br />
-      <h3>Select your power sources by plugging in the cables — you have up to 10 power lines.</h3>
+      <h2>Plug in your power lines. You can choose up to 10.</h2>
       <br />
-      <h3>
-        As the city’s power demand changes, use the controls to match production to demand.
-        <br />
-        Some power sources respond more quickly than others,
-        and power from the wind and sun will change depending on the weather.
-      </h3>
+      <h2>Select your power sources by plugging in the cables — you have up to 10 power lines.</h2>
       <br />
-      <h3>
-        Beware! Disconnecting a power line might cause an outage.
-        Keep an eye on your demand and efficiency.
-      </h3>
+      <br />
+      <h2>
+        <strong>Use the controls</strong>
+        {' '}
+        to change your energy output as the
+        {' '}
+        <strong>demand bar</strong>
+        {' '}
+        moves up and down.
+      </h2>
+      <br />
+      <br />
       <br />
     </ModalBody>
     <ModalFooter>
       <h1>
-        Ready to play? Press
+        Ready to play? PRESS
         {' '}
         <strong>START.</strong>
       </h1>
