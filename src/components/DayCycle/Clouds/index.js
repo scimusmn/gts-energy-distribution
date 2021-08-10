b28a06edf8c21be4987d02718389b8a2ad27d49f
trig-vee <tnordberg@smm.org>
2021-08-10 16:17:08 -0500

    fix: clouds unique key error

--- src/components/DayCycle/Clouds/index.js
+++ src/components/DayCycle/Clouds/index.js
@@ -1,15 +1,16 @@
 /* stylelint function-name-case: 0 */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NewKey } from '../../../utils';
 
 const Clouds = ({ wind, quantity }) => (
   <div className="clouds">
     {/* Displayed during little to no wind */}
-    {Array(quantity).fill(<div className={`cloud still ${(wind <= 7) ? 'show' : ''}`} />)}
+    {Array(quantity).fill(<div key={NewKey()} className={`cloud still ${(wind <= 7) ? 'show' : ''}`} />)}
     {/* Displayed during medium wind */}
-    {Array(quantity).fill(<div className={`cloud slow ${(wind > 7 && wind < 21) ? 'show' : ''}`} />)}
+    {Array(quantity).fill(<div key={NewKey()} className={`cloud slow ${(wind > 7 && wind < 21) ? 'show' : ''}`} />)}
     {/* Displayed during strong wind */}
-    {Array(quantity).fill(<div className={`cloud fast ${(wind >= 21) ? 'show' : ''}`} />)}
+    {Array(quantity).fill(<div key={NewKey()} className={`cloud fast ${(wind >= 21) ? 'show' : ''}`} />)}
   </div>
 );
 
--- src/components/ReadyPrompt/index.js
+++ src/components/ReadyPrompt/index.js
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 
 const ReadyPrompt = ({ show }) => (
   <h1 className={`ready-prompt ${show ? 'show' : ''}`}>
-    Ready?
     <br />
-    POWER YOUR CITY
+    Get ready to power your city!
   </h1>
 );
 
